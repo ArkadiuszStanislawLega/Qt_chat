8d1c04b65346bf1c199c9e8912c7a6151b46d369
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2023-07-13 20:17:37 +0200

    Update server class - added basic functions.

--- chat_server/server.cpp
+++ chat_server/server.cpp
@@ -1,6 +1,30 @@
 #include "server.h"
+#define MAX_USERS_CONNECTED 200
 
 Server::Server(QObject *parent) : QTcpServer(parent)
 {
+    this->_pool.setMaxThreadCount(MAX_USERS_CONNECTED);
+    qInfo() << "Max number of users: " << MAX_USERS_CONNECTED;
+}
+
+void Server::start(qint16 port)
+{
+    if(this->listen(QHostAddress::Any, port)){
+        qInfo() << "Server started on port: " << port;
+    } else {
+        qWarning() << this->errorString();
+    }
+}
+
+void Server::quit()
+{
+    this->close();
+    qInfo() << "Server stopped.";
+}
+
+void Server::incomingConnection(qintptr handle)
+{
+    qInfo() << "Incoming Connection " << handle << " " << QThread::currentThread();
+    //TODO: finish this whan client class created.
 
 }
--- chat_server/server.h
+++ chat_server/server.h
@@ -3,12 +3,25 @@
 
 #include <QObject>
 #include <QTcpServer>
+#include <QThreadPool>
+#include <QDebug>
 
 class Server : public QTcpServer
 {
     Q_OBJECT
 public:
     Server(QObject *parent = nullptr);
+
+public slots:
+    void start(qint16 port);
+    void quit();
+
+private:
+    QThreadPool _pool;
+
+    // QTcpServer interface
+protected:
+    virtual void incomingConnection(qintptr handle) Q_DECL_OVERRIDE;
 };
 
 #endif // SERVER_H
